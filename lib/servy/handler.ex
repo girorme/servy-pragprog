7249735c60fe0f467f652002b87952065b8804f3
girorme <rodrigogirorme@gmail.com>
2021-08-29 02:20:03 -0300

    Add conv struct

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -8,6 +8,88 @@ defmodule Servy.Handler do
   import Servy.Parser, only: [parse: 1]
   import Servy.FileHandler, only: [handle_file: 2]
 
+  alias Servy.Conv
+
+  def start_server() do
+    request = """
+      GET /wildthings HTTP/1.1
+      Host: example.com
+      User-Agent: ExampleBrowser/1.0
+      Accept: */*
+
+      """
+
+      response = Servy.Handler.handle(request)
+
+      IO.puts response
+
+      request = """
+      GET /bears HTTP/1.1
+      Host: example.com
+      User-Agent: ExampleBrowser/1.0
+      Accept: */*
+
+      """
+      response = Servy.Handler.handle(request)
+
+      IO.puts response
+
+      request = """
+      GET /bigfoot HTTP/1.1
+      Host: example.com
+      User-Agent: ExampleBrowser/1.0
+      Accept: */*
+
+      """
+      response = Servy.Handler.handle(request)
+
+      IO.puts response
+
+      request = """
+      GET /bears/1 HTTP/1.1
+      Host: example.com
+      User-Agent: ExampleBrowser/1.0
+      Accept: */*
+
+      """
+      response = Servy.Handler.handle(request)
+
+      IO.puts response
+
+      request = """
+      GET /bears?id=1 HTTP/1.1
+      Host: example.com
+      User-Agent: ExampleBrowser/1.0
+      Accept: */*
+
+      """
+      response = Servy.Handler.handle(request)
+
+      IO.puts response
+
+      request = """
+      GET /about HTTP/1.1
+      Host: example.com
+      User-Agent: ExampleBrowser/1.0
+      Accept: */*
+
+      """
+      response = Servy.Handler.handle(request)
+
+      IO.puts response
+
+      request = """
+      GET /bears/new HTTP/1.1
+      Host: example.com
+      User-Agent: ExampleBrowser/1.0
+      Accept: */*
+
+      """
+      response = Servy.Handler.handle(request)
+
+      IO.puts response
+  end
+
   @doc "Transforms the request into a response"
   def handle(request) do
     request
@@ -20,152 +102,65 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def emojify(%{status: 200} = conv) do
+  def emojify(%Conv{status: 200} = conv) do
     emojies = String.duplicate("🎉", 5)
     body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
 
     %{ conv | resp_body: body}
   end
 
-  def emojify(conv), do: conv
+  def emojify(%Conv{} = conv), do: conv
 
-  def route(%{method: "GET", path: "/wildthings"} = conv) do
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(%{method: "GET", path: "/bears"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     %{ conv | status: 200, resp_body: "A, B, C" }
   end
 
-  def route(%{method: "GET", path: "/bears/new"} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
     Path.expand("../../pages", __DIR__)
       |> Path.join("form.html")
       |> File.read
       |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
     Path.expand("../../pages", __DIR__)
       |> Path.join("about.html")
       |> File.read
       |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/pages/" <> file} = conv) do
+  def route(%Conv{method: "GET", path: "/pages/" <> file} = conv) do
       @pages_path
       |> Path.join(file <> ".html")
       |> File.read
       |> handle_file(conv)
   end
 
-  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
     %{ conv | status: 200 , resp_body: "Bear #{id}"}
   end
 
-  def route(%{path: path} = conv) do
+  def route(%Conv{path: path} = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here"}
   end
 
-  def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
+  def route(%Conv{method: "DELETE", path: "/bears/" <> _id} = conv) do
     %{ conv | status: 403, resp_body: "Bears must never be deleted!"}
   end
 
-  def format_response(conv) do
+  def format_response(%Conv{} = conv) do
+    full_status = Conv.full_status(conv)
+
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{full_status}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
-
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
-  end
 end
-
-request = """
-GET /wildthings HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /bears HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /bigfoot HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /bears/1 HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /bears?id=1 HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /about HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-response = Servy.Handler.handle(request)
-
-IO.puts response
-
-request = """
-GET /bears/new HTTP/1.1
-Host: example.com
-User-Agent: ExampleBrowser/1.0
-Accept: */*
-
-"""
-response = Servy.Handler.handle(request)
-
-IO.puts response
--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -1,24 +1,26 @@
 defmodule Servy.Plugins do
   require Logger
 
-  def track(%{status: 404, path: path} = conv) do
+  alias Servy.Conv
+
+  def track(%Conv{status: 404, path: path} = conv) do
     Logger.warn "Warning: #{path} is on the loose!"
     conv
   end
 
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 
-  def rewrite_path(%{path: path} = conv) do
+  def rewrite_path(%Conv{path: path} = conv) do
     regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
     captures = Regex.named_captures(regex, path)
     rewrite_path_captures(conv, captures)
   end
 
-  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+  def rewrite_path_captures(%Conv{} = conv, %{"thing" => thing, "id" => id}) do
     %{ conv | path: "/#{thing}/#{id}" }
   end
 
-  def rewrite_path_captures(conv, nil), do: conv
+  def rewrite_path_captures(%Conv{} = conv, nil), do: conv
 
-  def log(conv), do: IO.inspect conv
+  def log(%Conv{} = conv), do: IO.inspect conv
 end
