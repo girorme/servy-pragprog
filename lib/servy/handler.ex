fd2879f7abdaae9aa7f4d13a7d69b4d36df8b01b
girorme <rodrigo.girorme@viavarejo.com.br>
2021-09-13 21:26:04 -0300

    add bear controller

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,5 +1,7 @@
 defmodule Servy.Handler do
 
+  alias Servy.BearController
+
   @moduledoc "Handles HTTP requests."
 
   @pages_path Path.expand("../../pages", __DIR__)
@@ -131,7 +133,7 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/bears"} = conv) do
-    %{ conv | status: 200, resp_body: "A, B, C" }
+    BearController.index(conv)
   end
 
   def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
@@ -156,11 +158,12 @@ defmodule Servy.Handler do
   end
 
   def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
-    %{ conv | status: 200 , resp_body: "Bear #{id}"}
+    params = Map.put(conv.params, "id", id)
+    BearController.show(conv, params)
   end
 
   def route(%Conv{method: "POST", path: "/bears"} = conv) do
-    %{ conv | status: 200 , resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}"}
+    BearController.create(conv, conv.params)
   end
 
   def route(%Conv{path: path} = conv) do
