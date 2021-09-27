4f09866cff022d43b660aba851c583962977c050
girorme <rodrigogirorme@gmail.com>
2021-09-27 02:12:36 -0300

    improve pledge_server

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -2,6 +2,7 @@ defmodule Servy.PledgeServer do
 
   @name :pledge_server
 
+  #client
   def start do
     IO.puts "Starting the pledge server..."
     pid = spawn(__MODULE__, :listen_loop, [[]])
@@ -9,6 +10,22 @@ defmodule Servy.PledgeServer do
     pid
   end
 
+  def create_pledge(name, amount) do
+    send @name, {self(), :create_pledge, name, amount}
+    receive do {:response, status} -> status end
+  end
+
+  def recent_pledges do
+    send @name, {self(), :recent_pledges}
+    receive do {:response, pledges} -> pledges end
+  end
+
+  def total_pledged do
+    send @name, {self(), :total_pledged}
+    receive do {:response, total} -> total end
+  end
+
+  #server
   def listen_loop(state) do
     receive do
       {sender, :create_pledge, name, amount} ->
@@ -20,17 +37,14 @@ defmodule Servy.PledgeServer do
       {sender, :recent_pledges} ->
         send sender, {:response, state}
         listen_loop(state)
+      {sender, :total_pledged} ->
+        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
+        send sender, {:response, total}
+        listen_loop(state)
+      unexpected ->
+        IO.puts "Unexpected message: #{inspect unexpected}"
+        listen_loop(state)
     end
-  end
-
-  def create_pledge(name, amount) do
-    send @name, {self(), :create_pledge, name, amount}
-    receive do {:response, status} -> status end
-  end
-
-  def recent_pledges) do
-    send @name, {self(), :recent_pledges}
-    receive do {:response, pledges} -> pledges end
   end
 
   defp send_pledge_to_service(_name, _amount) do
