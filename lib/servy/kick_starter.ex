88cb3ac9a6c177d31ae92942899b8720c1eb80fa
girorme <rodrigogirorme@gmail.com>
2021-10-24 16:48:32 -0300

    Add mod/starting to servy supersivor

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -20,7 +20,8 @@ defmodule Servy.KickStarter do
 
   defp start_server do
     IO.puts("Starting the HTTP server")
-    server_pid = spawn_link(Servy.HttpServer, :start, [4000])
+    port = Application.get_env(:servy, :port)
+    server_pid = spawn_link(Servy.HttpServer, :start, [port])
     Process.register(server_pid, :http_server)
     server_pid
   end
